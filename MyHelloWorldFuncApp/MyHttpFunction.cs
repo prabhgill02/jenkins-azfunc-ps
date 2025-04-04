97484eeefd3cb4846474b2d23a5bd316341bf0e6
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2025-04-04 18:54:52 -0400

    Added func

---
+++ MyHelloWorldFuncApp/MyHttpFunction.cs
@@ -0,0 +1,17 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyHelloWorldFuncApp
+{
+    public class MyHttpFunction
+    {
+
+        [Function("MyHttpFunction")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
---
+++ MyHelloWorldFunctApp.Tests/MyHelloWorldFunctAppTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyHelloWorldFuncApp;
+using Xunit;
+using Moq;
+
+public class MyHttpFunctionTests
+{
+    [Fact]
+    public void Run_ShouldReturn_OkObjectResult_WithExpectedMessage()
+    {
+        // Arrange
+        var mockHttpRequest = new Mock<HttpRequest>();
+
+        // Act
+        var result = MyHttpFunction.Run(mockHttpRequest.Object) as OkObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal("Welcome to Azure Functions!", result.Value);
+    }
+}
